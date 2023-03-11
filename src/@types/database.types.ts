5638100a203bb2008e4fd4764fa3618483b64807
Carlos Bermejo Pérez <carlosbermejop@gmail.com>
2023-03-11 17:32:44 +0100

    chore: forgot types update

--- src/@types/database.types.ts
+++ src/@types/database.types.ts
@@ -34,7 +34,32 @@ export interface Database {
   }
   public: {
     Tables: {
-      [_ in never]: never
+      profiles: {
+        Row: {
+          avatar_url: string | null
+          full_name: string | null
+          id: string
+          updated_at: string | null
+          username: string | null
+          website: string | null
+        }
+        Insert: {
+          avatar_url?: string | null
+          full_name?: string | null
+          id: string
+          updated_at?: string | null
+          username?: string | null
+          website?: string | null
+        }
+        Update: {
+          avatar_url?: string | null
+          full_name?: string | null
+          id?: string
+          updated_at?: string | null
+          username?: string | null
+          website?: string | null
+        }
+      }
     }
     Views: {
       [_ in never]: never
